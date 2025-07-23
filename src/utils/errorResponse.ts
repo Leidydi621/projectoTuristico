c921ed64f7c6aec18d8dcb511a715b40eafd49ba
Shago01 <kgsanty2001@gmail.com>
2025-07-23 18:02:31 -0500

    fix - refactorización del majeno de errores y respuesta de error

--- src/utils/errorResponse.ts
+++ src/utils/errorResponse.ts
@@ -1,8 +1,7 @@
 import { Response } from "express";
 
 /**
- * 
-  @param res Reponse de express
+ * @param res Reponse de express
  * @param code Número que representa el código de error HTTP
  * @param message Mensaje descriptivo de error
  * @param details Objeto con información adicional 
